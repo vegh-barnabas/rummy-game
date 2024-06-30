7c01eee30f7e31c146b62a7584b2325f0f4507b5
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2024-06-30 20:20:43 +0200

    feat(game-page): add base of game page

---
+++ src/app/game-page/game-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-page.component.html',
+  styleUrl: './game-page.component.scss'
+})
+export class GamePageComponent {}
