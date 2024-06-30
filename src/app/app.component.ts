505d922f81f025f275db3d0a2cb57c4edf3eb567
vegh-barnabas <62122932+vegh-barnabas@users.noreply.github.com>
2024-06-30 20:21:09 +0200

    feat(app): add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GamePageComponent } from './game-page/game-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'game', component: GamePageComponent }
+];
